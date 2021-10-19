23570f0359e38d5cb936e0d7065b4416e8ecb47e
royfds <74107306+royfds@users.noreply.github.com>
2021-10-20 01:53:44 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ API_ID = environ.get('API_ID', '6')
 API_HASH = environ.get('API_HASH', 'eb06d4abfb49dc3eeb1aeb98ae0f581e')
 BOT_TOKEN = environ.get('BOT_TOKEN')
 PDISK_API_KEY = environ.get('PDISK_API_KEY')
-CHANNEL = environ.get('CHANNEL', 'MyTestBotZ')
+CHANNEL = environ.get('CHANNEL', 'Moviezbarn')
 THUMB_URL = environ.get('THUMB_URL', '')
 
 bot = Client('pdisk bot',
@@ -27,7 +27,7 @@ bot = Client('pdisk bot',
 async def start(bot, message):
     await message.reply(
         f"**Hiya 👋{message.chat.first_name}!**\n\n"
-        "**A Simple PDisk Uploader Bot.\n\n➠ Send Me Any Direct Link, YouTube Link Or Video Link  I Will Upload To PDisk And Give Direct Link\n\nMade With❤BY @MyTestBotZ**")
+        "**A Simple PDisk Uploader Bot.\n\n➠ Send Me Any Direct Link, YouTube Link Or Video Link  I Will Upload To PDisk And Give Direct Link\n\nMade With❤BY @Moviezbarn**")
 
     
 @bot.on_message(filters.text & filters.private)
@@ -150,7 +150,7 @@ async def addFooter(str):
 <b> Note : Your Video File is Available on Above LINK ones Upload Process is Complete, it Take Time Depend on Your File Size & My Server Upload Speed
 So,be Patient </b>  😴😴😴😴     
 ━━━━━━━━━━━━━━━
-⦿ Made With♥️BY @OO7ROBot
+⦿ Made With 💝 BY @moviezbarn
 ━━━━━━━━━━━━━━━
 ✪ »JOIN CHANNEL ➡️ t.me/""" + CHANNEL
     return str + footer
